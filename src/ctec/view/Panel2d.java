e1a64852762194c32a6c965438a9bcaddd4b28b2
Traitortots <jker3169@740s-pl-844.local>
2016-02-19 09:22:49 -0700

    started setting up my panel

--- src/ctec/view/Panel2d.java
+++ src/ctec/view/Panel2d.java
@@ -7,9 +7,35 @@ import ctec.controller.Controller2d;
 public class Panel2d extends JPanel
 {
 
+	private Controller2d baseController;
+	
 	public Panel2d(Controller2d baseController)
 	{
-		// TODO Auto-generated constructor stub
+		this.baseController = baseController;
+		
+		
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+
+	private void setupPanel()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void setupLayout()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void setupListeners()
+	{
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
