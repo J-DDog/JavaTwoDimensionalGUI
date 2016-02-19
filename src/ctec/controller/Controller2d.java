0b2c4ecdbd07a265022fbdba4b7a26898603a3ba
Traitortots <jker3169@740s-pl-844.local>
2016-02-19 09:30:54 -0700

    added uml to the view package and started crating the controllers variables

--- src/ctec/controller/Controller2d.java
+++ src/ctec/controller/Controller2d.java
@@ -1,8 +1,20 @@
 package ctec.controller;
 
+import ctec.model.Egg;
+import ctec.view.Frame2d;
+
 public class Controller2d
 {
 	
+	private Frame2d baseFrame;
+	private Egg[][] EggCaton;
+	
+	public Controller2d()
+	{
+		baseFrame = new Frame2d(this);
+		
+		
+	}
 	
 	public void start()
 	{
---
+++ src/ctec/model/Egg.java
@@ -0,0 +1,14 @@
+package ctec.model;
+
+public class Egg
+{
+	private Boolean isBrownEgg, isLarge, isCraked;
+	
+	public Egg(Boolean isBrownEgg, Boolean isLarge, Boolean isCraked)
+	{
+		this.isBrownEgg = isBrownEgg;
+		this.isLarge = isLarge;
+		this.isCraked = isCraked;
+	}
+	
+}
